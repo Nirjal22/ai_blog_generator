a19a3a4102d1d8535a3b494b3481cadc35d59b5e
Nirjal22 <bistanirjal978@gmail.com>
2024-11-20 17:22:07 +0545

    django-app

--- blog_generator/urls.py
+++ blog_generator/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_view, name='login'),
     path('signout/', views.signout, name='signout'),
 ]
